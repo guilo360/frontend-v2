81ad4b47efd793d91fedf4bd46dbf7f357ceb24a
kain <kain@nixos.com>
2024-10-14 15:23:56 +1300

    js and Readme.md

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ### Kain and Andrew's Assignment
-This is v2 because version 1 relied on a bunch of stupid dependencies. This is just html and js because \n
+This is v2 because version 1 relied on a bunch of stupid dependencies. This is just html and js because:
 
 Less is more
 
 ## Todo
-sort out nested if-else in roulette.js and replace with a switch statement to validate input - low priority
+sort out nested if-else in roulette.js and replace with a switch statement to validate input - low priority 
+make a better random function - this version just rounds the result from Math.random() and is not uniform :( 
 
 ## DEBUG TOOLS
 
--- js/roullette.js
+++ js/roullette.js
@@ -9,9 +9,36 @@ form.addEventListener('submit', function(event) {
   // Get the value of the input field
   const selectedNumber = document.getElementById('selected-number').value;
   const betAmount = document.getElementById('bet-amount').value;
+  const rouletteResultDiv = document.getElementById('roulette-result');
 
-  // Do something with the input value (e.g., log it to the console)
+	// This logs to console, then validates input. 
+	// This *should* be replaced with a switch statement but I'm a bad programmer
+	// yeah nested if-else loops - BAD - added to to-do list
+	// also this validation isnt fully necessary, but is a good idea!
   console.log('Form submitted:', selectedNumber, betAmount);
+	if (betAmount <= 0) {
+		console.log('invalid bet: below 0');
+		return
+	} else if (betAmount >= 100) {
+		console.log('invalid bet: exceeds my aribtrary limit of 100 tBNB');
+		return
+	} else if (selectedNumber == null) {
+		console.log('please bet on a number!');
+		return
+	} else {
+		//Handle the win case first
+		//THIS IS NOT A GOOD RANDOM FUNCTION _ NOT UNIFORM
+		let roll = Math.random() * 37;
+		let rollRounded = Math.round(roll);
+		if (selectedNumber == roll) {
+		rouletteResultDiv.innerHTML = `Congrats! You bet on ${selectedNumber} and rolled ${rollRounded}`;
+		} else {
+		rouletteResultDiv.innerHTML = `Hahah loser, you bet on ${selectedNumber} and rolled ${rollRounded}`;
+		}
+	}
+
+
+
 
   // You can also add further processing here (e.g., send data to a server)
 });
